65e28da578a5de352f15a72807a56ec9b3a00ab4
Lynn <lynnpbcc@yahoo.com>
2021-02-19 20:26:14 -0500

    Operator

---
+++ CSharpOperatorsExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpOperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            // a /= 4;
+
+            //Console.WriteLine(a);
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+        }
+    }
+}
